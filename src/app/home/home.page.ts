58d7d8f02e999d18a36a8bae8e6c891f37307285
Muhammad Syazani <syazanifaudzi@gmail.com>
2024-08-04 12:22:01 +0800

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  weight:number = 70
+  height:number = 170
+  bmi:number = 0
+  message:string = ""
 
+  constructor() {
+    this.bmi = this.roundTo(this.bmi, 2);
+  }
+  
+  roundTo(num: number, decimalPlaces: number): number {
+    const factor = Math.pow(10, decimalPlaces);
+    return Math.round(num * factor) / factor;
+  }
+
+  calculationBMI() {
+    this.bmi = Math.round(this.weight / Math.pow((this.height/100), 2))
+    if(this.bmi >= 18.5 && this.bmi <= 24.9){
+      this.message = "Normal"
+    }
+    else if (this.bmi >= 25 && this.bmi <= 29.9){
+      this.message = "Overweight"
+    }
+    else if (this.bmi >= 30 && this.bmi <= 39.9){
+      this.message = "Obese"
+    }
+    else if (this.bmi >= 40){
+      this.message = "Morbidly Obese"
+    }
+    else{
+      this.message = "Underweight"
+    }
+  }
 }
